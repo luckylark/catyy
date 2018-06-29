702100aeddb91cf3ede7996e9760c6f83cb3baf8
honglin li <lucky_lark@163.com>
2018-06-29 17:34:19 +0800

    complete profile part

--- app/app/user/view_profile.py
+++ app/app/user/view_profile.py
@@ -2,7 +2,7 @@ from . import user
 from ..models.user import User
 from flask import render_template, flash, redirect, request, url_for
 from flask_login import login_required, current_user
-from ..forms.profile import PhotoForm
+from ..forms.profile import PhotoForm, EditProfileForm
 import os
 from ..extentions import avatarUser, db
 from ..tools.photo import resize
@@ -38,9 +38,38 @@ def profile(id):
     return render_template('profile.html', user = user)
 
 
+@user.route('/profile/details/<int:id>')
+def profile_details(id):
+    user = User.query.get_or_404(id)
+    return render_template('profile_details.html', user = user)
+
+
 @login_required
 @user.route('/edit_profile', methods=['GET', 'POST'])
 def edit_profile():
-    return render_template('edit_profile.html')
+    form = EditProfileForm()
+    if request.method == 'GET':
+        form.username.data = current_user.username
+        form.gender.data = current_user.gender
+        form.birthday.data = current_user.birthday
+        form.about_me.data = current_user.about_me
+        form.name.data = current_user.name
+        form.phone.data = current_user.phone
+        form.id_number.data = current_user.id_number
+        form.address.data = current_user.address
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.gender = form.gender.data
+        current_user.birthday = form.birthday.data
+        current_user.about_me = form.about_me.data
+        current_user.phone = form.phone.data
+        current_user.name = form.name.data
+        current_user.id_number = form.id_number.data
+        current_user.address = form.address.data
+        db.session.add(current_user)
+        flash('个人资料编辑成功')
+        return redirect(url_for('user.profile_details', id=current_user.id))
+    return render_template('edit_profile.html', form=form)
+
 
 
