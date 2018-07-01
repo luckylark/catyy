914129e38732f5a4c0f9777723be31dcaa778ac3
honglin li <lucky_lark@163.com>
2018-07-01 20:24:35 +0800

    add gevent server

---
+++ app/gserver.py
@@ -0,0 +1,9 @@
+from gevent.pywsgi import WSGIServer
+from app import create_app
+import os
+
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+server=WSGIServer(('', 80), app)
+server.serve_forever()
